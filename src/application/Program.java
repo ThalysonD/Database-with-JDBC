cb19e1be3c9604eee078a40d08eb1dc057675b78
Thalyson Dornelas <thalysondornelasgomes14@gmail.com>
2023-04-01 23:04:12 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new ID = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Updated Completed!");
+		
 }
 }
